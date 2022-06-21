4103e1b1d7cf69ae90b6c5458bf425ddd3edfc01
tommy <tcatty7@gmail.com>
2022-06-21 14:26:17 -0400

    c

---
+++ script.js
@@ -0,0 +1,7 @@
+function cat() {
+    let person =
+    prompt("please enter your name", "darth vader");
+if (person !=null) {
+    document.getElementById("red").innerHTML = "Welcome to my website " + person + " I hope you like it";
+}
+}
