65c67d1514aac17ee66a8f36ac9f23d8f7412c6b
tommy <tcatty7@gmail.com>
2022-07-01 11:49:59 -0400

    to show mommy

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ function cat() {
 const a = document.getElementById('f').innerHTML
 alert(a)
 }
+var a = `hello i am the developer 'tcat2022' and this was a great project and I hope you can learn from and use my code`
+console.log(a)
