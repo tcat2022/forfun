92da733f53522b58e43bb128d182c702630a2621
tommy <tcatty7@gmail.com>
2022-12-30 13:25:24 -0500

    g

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function rob() {
-    alert('This is Jack taking a goalkick.\n\n \\"keep clicking things and discover hidden messages"/')
-}
-function cat() {
-const a = document.getElementById('f').innerHTML
-alert(a)
-}
-var b = `hello i am the developer 'tcat2022' and this was a great project and I hope you can learn from and use my code`
-console.log(b)
-const e = "hello console"
-console.log(e);
+
