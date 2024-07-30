c9500b29dc34ba77c0803e740f98bf3985ebf50d
Tommey <Tcatty7@gmail.com>
2024-07-30 15:48:17 -0400

    test 2

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const name = document.getElementById('name');
-const lName = document.getElementById('lName');
-const email = document.getElementById('email');
-const subject = document.getElementById('subject');
-const text = document.getElementById('text');
+const name1 = document.getElementById('name').innerText;
+const email = document.getElementById('email').innerText;
+const subject = document.getElementById('subject').innerText;
+const text = document.getElementById('text').innerText;
 const submit = document.getElementById('submit')
+
+let body = "Name: " + name1 + "<br/>Email: " + email + "<br/>message: " + text;
 
 submit.addEventListener('click', (e) => {
     e.preventDefault()
@@ -13,10 +14,10 @@ submit.addEventListener('click', (e) => {
         Password : "96915101A454B97FA5208BAD4C9AAA3560E6",
         To : 'tcatty7@gmail.com',
         From : "tcatty7@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        Subject : subject,
+        Body : body
     }).then(
-      message => alert(message)
+      message => alert(thx)
     );
 console.log('hey')
 })
